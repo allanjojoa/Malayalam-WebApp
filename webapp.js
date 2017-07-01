9773eb3d49a29891e99b0b6cb714ec3594b0ed88
Allan <allanjojoa@gmail.com>
2017-07-01 17:39:52 +0530

    New Bootstrap Page

--- webapp.js
+++ webapp.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var login_status=0;
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -108,9 +109,11 @@ var lesson = {
 
 app.get('/', function(req, res) {
   // res.sendFile(path.join(__dirname,'ui','main.html'))
-  // res.send(createTemplate(lesson['lesson-one']));
-  res.redirect('login');
+  if(login_status>0)
+  res.send(createTemplate(lesson['lesson-one']));
+  else res.redirect('login');
 });
+app.use(express.static(__dirname + '/ui'));
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -121,17 +124,22 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/home', function(req, res) {
+  if(login_status>0)
   res.send(createTemplate(lesson['lesson-one']));
+  else res.redirect('login');
 });
 
 app.post("/login_post", function(req, res) {
   if (req.method == 'POST') {
     //var sql = "select * from users where email='allanjojoa@gmail.com' and password='allan';";
-    var sql = "select * from users where email='"+req.body.userid+"' and password='"+req.body.password+"';";
-    con.query(sql, function (err, result) {
-      numRows=result.length;
+    var sql = "select * from users where email='" + req.body.userid + "' and password='" + req.body.password + "';";
+    con.query(sql, function(err, result) {
+      numRows = result.length;
       console.log(numRows);
-      if(numRows>0) res.redirect('home');
+      if (numRows > 0) {
+        login_status=1;
+        res.redirect('home');
+      }
       else res.redirect('login');
     });
 
@@ -144,34 +152,26 @@ app.get('/signup', function(req, res) {
 
 app.post("/signup_post", function(req, res) {
   if (req.method == 'POST') {
-    var sql = "INSERT INTO users (username,email,password) VALUES ('"+req.body.fname+"','"+req.body.email+"','"+req.body.password+"')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 new user");
-  });
+    var sql = "INSERT INTO users (username,email,password) VALUES ('" + req.body.fname + "','" + req.body.email + "','" + req.body.password + "')";
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+      console.log("1 new user");
+    });
     res.redirect('login');
   }
 });
 
-app.get('/ui/js/boostrap.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'js', 'bootstrap.min.js'))
-});
-
 app.get('/:lessonName', function(req, res) {
-  var lessonName = req.params.lessonName;
-  res.send(createTemplate(lesson[lessonName]));
+  if(login_status>0){
+    var lessonName = req.params.lessonName;
+    res.send(createTemplate(lesson[lessonName]));
+  }
+  else res.redirect('login');
 });
-
-app.get('/ui/css/bootstrap.min.css', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'css', 'bootstrap.min.css'))
-});
-app.get('/ui/js/jquery.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'js', 'jquery.min.js'))
-})
 
 var port = 8080;
 app.listen(8080, function() {
-  console.log(`Port ${port}!`);
+  console.log(`Listening http://localhost:${port}`);
 });
 
 function createTemplate(data) {
@@ -181,112 +181,135 @@ function createTemplate(data) {
   var description = data.description;
 
   var htmlTemplate = `
-    <!DOCTYPE html>
-    <html lang="en">
+  <!DOCTYPE html>
+  <html lang="en">
 
-    <head>
-      <style>
-        .btn-group.special {
-          display: flex;
-        }
+  <head>
+    <style>
+    .embeded-video {
+      position: relative
+    }
+    .embeded-video .ratio-img {
+      display: block;
+      width: 100% !important;
+      height: auto !important;
+    }
 
-        .special .btn {
-          flex: 1
-        }
+    .embeded-video IFRAME {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        .embeded-video {
-          position: relative
-        }
-        .navbar {
-              margin-bottom: 0;
-              background-color: #f4511e;
-              // z-index: 9999;
-              // border: 0;
-              // font-size: 12px !important;
-              // line-height: 1.42857143 !important;
-              // letter-spacing: 4px;
-              // border-radius: 0;
-              // font-family: Montserrat, sans-serif;
-          }
-          .navbar li a, .navbar .navbar-brand {
-              color: #fff !important;
-          }
-          .navbar-nav li a:hover, .navbar-nav li.active a {
-              color: #f4511e !important;
-              background-color: #fff !important;
-          }
-          .navbar-default .navbar-toggle {
-              border-color: transparent;
-              color: #fff !important;
-          }
-        .jumbotron
-        {
-        /*padding-top: 0px;
-        padding-bottom:0px;*/
-        margin: 0;
-        // background-image:url('http://www.indiaholidayarchitects.com/wp-content/uploads/2014/12/Kerala-India-travel-background-green-tea-plantations-in-Munnar-Kerala-India-copy-1024x550.jpg') ;
-        // background-size: cover;
-        // background-color: black;
-        position: relative;
-        height:150px;
-         }
+    <title>ആദ്യാക്ഷര</title>
 
-        .embeded-video .ratio-img {
-          display: block;
-          width: 100% !important;
-          height: auto !important;
-        }
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/nivo-lightbox.css" rel="stylesheet" />
+    <link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+    <link href="css/flexslider.css" rel="stylesheet" />
+    <link href="css/animate.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="color/default.css" rel="stylesheet">
+    <!-- =======================================================
+          Theme Name: Shuffle
+          Theme URL: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/
+          Author: BootstrapMade
+          Author URL: https://bootstrapmade.com
+      ======================================================= -->
 
-        .embeded-video IFRAME {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-      </style>
-      <title>${title}</title>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="/ui/css/bootstrap.min.css">
-      <script src="/ui/js/jquery.min.js"></script>
-      <script src="/ui/js/bootstrap.min.css"></script>
-    </head>
+  </head>
 
-    <body style="background:white">
-      <nav class="navbar navbar-default ">
-        <div class="jumbotron" style="background-color:#ff6700">
-          <div class="container">
-          <h1>ആദ്യാക്ഷരം</h1>
-            <p>LEARN MALAYALAM THE EASY WAY!!!</p>
-            <!-- <p>Paragraph Paragraph Paragraph Paragraph Paragraph</p> -->
+  <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+    <section id="intro" class="home-slide text-light">
+
+      <!-- Carousel -->
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="img/slide1.jpg" alt="First slide">
+            <!-- Static Header -->
+            <div class="header-text hidden-xs">
+              <div class="col-md-12 text-center">
+                <h2>
+                                <span>ആദ്യാക്ഷരം</span>
+                              </h2>
+                <br>
+                <h3>
+                                <span>LEARN MALAYALAM THE EASY WAY!!!</span>
+                              </h3>
+                <br>
+            </div>
+            <!-- /header-text -->
+          </div>
           </div>
         </div>
-        <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Home</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="/lesson-one">Lesson 1</a></li>
-            <li><a href="/lesson-two">Lesson 2</a></li>
-            <li><a href="/lesson-three">Lesson 3</a></li>
-            <li><a href="/lesson-four">Lesson 4</a></li>
-            <li><a href="/lesson-five">Lesson 5</a></li>
-            <li><a href="/lesson-six">Lesson 6</a></li>
-            <li><a href="/lesson-seven">Lesson 7</a></li>
-            <li><a href="/lesson-eight">Lesson 8</a></li>
-            <li><a href="/lesson-nine">Lesson 9</a></li>
-            <li><a href="/lesson-ten">Lesson 10</a></li>
-          </ul>
-        </div>
       </div>
-    </nav>
+      <!-- /carousel -->
+
+    </section>
+    <!-- /Section: intro -->
+
+
+    <!-- Navigation -->
+    <div id="navigation">
+      <nav class="navbar navbar-custom" role="navigation">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-2">
+              <div class="site-logo">
+                <a href="/home" class="brand">ആദ്യാക്ഷരം</a>
+              </div>
+            </div>
+
+
+            <div class="col-md-10">
+
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+                                                  <i class="fa fa-bars"></i>
+                                                  </button>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="menu">
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="/lesson-one#lesson_section">Lesson 1</a></li>
+                  <li><a href="/lesson-two#lesson_section">Lesson 2</a></li>
+                  <li><a href="/lesson-three#lesson_section">Lesson 3</a></li>
+                  <li><a href="/lesson-four#lesson_section">Lesson 4</a></li>
+                  <li><a href="/lesson-five#lesson_section">Lesson 5</a></li>
+                  <li><a href="/lesson-six#lesson_section">Lesson 6</a></li>
+                  <li><a href="/lesson-seven#lesson_section">Lesson 7</a></li>
+                  <li><a href="/lesson-eight#lesson_section">Lesson 8</a></li>
+                  <li><a href="/lesson-nine#lesson_section">Lesson 9</a></li>
+                  <li><a href="/lesson-ten#lesson_section">Lesson 10</a></li>
+                </ul>
+              </div>
+              <!-- /.Navbar-collapse -->
+
+            </div>
+          </div>
+        </div>
+        <!-- /.container -->
+      </nav>
+    </div>
+    <!-- /Navigation -->
+
+    <!-- Section: about -->
+    <section id="lesson_section" class="home-section color-dark bg-white">
       <div class="container-fluid">
         <div class="modal-body row">
           <div class="col-md-8">
@@ -302,9 +325,58 @@ function createTemplate(data) {
             <p>${description}</p </div>
           </div>
         </div>
-    </body>
+      </div>
+    </section>
+    <!-- /Section: about -->
 
-    </html>
+
+
+
+
+
+
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+
+            <div class="text-center">
+              <a href="#intro" class="totop"><i class="fa fa-angle-up fa-3x"></i></a>
+              <p>&copy; Shuffle Theme - All Rights Reserved</p>
+              <div class="credits">
+                <!--
+                                  All the links in the footer should remain intact.
+                                  You can delete the links only if you purchased the pro version.
+                                  Licensing information: https://bootstrapmade.com/license/
+                                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Shuffle
+                              -->
+                <a href="https://bootstrapmade.com/">Free Bootstrap Themes</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.scrollTo.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/nivo-lightbox.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="contactform/contactform.js"></script>
+
+  </body>
+
+  </html>
 
                 `;
   return htmlTemplate;
